fd5f377e66cd6a457cea80cf0b4b3201ae042d9d
Vassardog77 <benmoxon256@gmail.com>
2023-02-21 17:58:40 -0700

    added analytics charjs

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,7 @@
 let posts = (posts = [], action) => {
     switch (action.type) {                 //shows what to do in each function case     
         case 'FETCH_ALL':
-            return action.payload
+            return action.payload       //payload comes from payload in actions
         case 'CREATE':
             return [...posts, action.payload]
         default:
