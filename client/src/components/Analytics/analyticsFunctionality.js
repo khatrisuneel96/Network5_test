3d75444a33d2b4f26c7dda9f7bd3faa1972b348e
Vassardog77 <benmoxon256@gmail.com>
2023-02-21 18:00:02 -0700

    added analytics and chartjs

--- client/src/components/Analytics/analyticsFunctionality.js
+++ client/src/components/Analytics/analyticsFunctionality.js
@@ -1,11 +1,45 @@
 import React from 'react';
-
+import { Line, Pie, Bar } from 'react-chartjs-2'
+import { Chart as Chartjs, LineElement, CategoryScale, LinearScale, PointElement,
+ArcElement, Tooltip, Legend, BarElement} from 'chart.js'
+Chartjs.register( LineElement, CategoryScale, LinearScale, PointElement, ArcElement, Tooltip, Legend, BarElement)
 function analyticsFunctionality(props) {
+    //line chart data
+    const data = {
+        labels: ['Mon', 'Tue', 'Wed'],
+        datasets: [{
+            label: 'Sales',
+            data: [1,9,4],
+            backgroundColor: ['#ffe709', '#826af0','#19bb65', '#ffe709'],
+            borderColor: 'black',
+            pointBorderColor: '',
+            //fill: 'true'
+        }]
+    }
+    const options = {
+        plugins: {
+            legend: true
+        }
+    }
+    //pie chart data
+
+
     return (
-        <div>
-            Analyticss
+        <div className='chart'>
+            <Line   
+                data = {data}
+                options = {options}
+            ></Line>
+            <Pie   
+                data = {data}
+                options = {options}
+            ></Pie>
+            <Bar   
+                data = {data}
+                options = {options}
+            ></Bar>
         </div>
-    );
+    )
 }
 
 export default analyticsFunctionality;
--- client/src/components/Profile/profileFunctionality.js
+++ client/src/components/Profile/profileFunctionality.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ProfileFunctionality(props) {
     return (
         <div>
-            Profile
+            test
         </div>
     );
 }
