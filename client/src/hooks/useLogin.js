5e9c8b9d78f229525a1c0fe97f420c0200432afd
Vassardog77 <benmoxon256@gmail.com>
2023-03-11 18:12:09 -0700

    login/signup test2

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
+import axios from 'axios'
 
 export const useLogin = () => {
   const [error, setError] = useState(null)
@@ -10,8 +11,38 @@ export const useLogin = () => {
     setIsLoading(true)
     setError(null)
 
+    var data = JSON.stringify({ email, password })
+    var config = {
+      method: 'post',
+    maxBodyLength: Infinity,
+      url: 'http://localhost:5000/api/user/login',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data))
+       // save the user to local storage
+       const json = response.data
+       localStorage.setItem('user', JSON.stringify(json))
 
+       // update the auth context
+       dispatch({type: 'LOGIN', payload: json})
+ 
+       // update loading state
+       setIsLoading(false)
+    })
+    .catch(function (error) {
+      console.log(error)
+      setIsLoading(false)
+      setError(error)
+    });
+  
 
+/*
     const response = await fetch('http://localhost:5000/api/user/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
@@ -32,7 +63,7 @@ export const useLogin = () => {
 
       // update loading state
       setIsLoading(false)
-    }
+    }*/
   }
 
   return { login, isLoading, error }
