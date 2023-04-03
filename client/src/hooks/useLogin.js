5950f2cf73cb79f2397b7f0aae9411e8ac7cde13
Vassardog77 <benmoxon256@gmail.com>
2023-04-02 20:59:10 -0700

    new build 2

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -44,7 +44,7 @@ export const useLogin = () => {
   
 */
 
-    const response = await fetch('http://localhost:5000/api/user/login', {
+    const response = await fetch(base_url+'/api/user/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
