2b12f034b974b85d6add2a8135f20509b5ac6dd5
Vassardog77 <benmoxon256@gmail.com>
2023-04-09 17:53:26 -0700

    trying weird require thing (probably won't work)

--- server/index.js
+++ server/index.js
@@ -1,95 +1,72 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import { Server } from 'socket.io'   //importing socket.io
-import http from 'http'
-import path from 'path'
-const __dirname = path.resolve();
-import * as dotenv from 'dotenv'
-dotenv.config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
 mongoose.set('strictQuery', true);
 
-import postRoutes2 from './routes/posts.js'
-import loginRoutes from './routes/loginRoutes.js'
-import messageRoutes from './routes/messageRoutes.js'
-import pageRoutes from './routes/pageRoutes.js'
-import emailRoutes from './routes/emailRoutes.js'
-import calendarRoutes from './routes/calendarRoutes.js'
-import webhookRoutes from './routes/webhookRoutes.js'
-import analyticsRoutes from './routes/analyticsRoutes.js'
-import postRoutes from './routes/postRoutes.js'
-import userRoutes from './routes/user.js'
-import profileRoutes from './routes/profileRoutes.js'
+const postRoutes2 = require('./routes/posts');
+const loginRoutes = require('./routes/loginRoutes');
+const messageRoutes = require('./routes/messageRoutes');
+const pageRoutes = require('./routes/pageRoutes');
+const emailRoutes = require('./routes/emailRoutes');
+const calendarRoutes = require('./routes/calendarRoutes');
+const webhookRoutes = require('./routes/webhookRoutes');
+const analyticsRoutes = require('./routes/analyticsRoutes');
+const postRoutes = require('./routes/postRoutes');
+const userRoutes = require('./routes/user');
+const profileRoutes = require('./routes/profileRoutes');
 
-const app = express()
+const app = express();
+const server = http.createServer(app);
 
-const server = http.createServer(app)    //setting server
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"],
+  }, 
+});
 
-const io = new Server(server, {     //linking socket.io to server
-    cors: {
-        origin: "*", //if stuff doesn't work maybe set orgin * (used to be "https://localhost:3000","https://aaazzz.xyz")
-        methods: ["GET", "POST"],
-    }, 
-})
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
 
-//////////////////////ADDING IN WEIRD DEPLOYMENT CODE//////////////////////
-/*const _dirname = path.dirname("")
-const buildPath = path.join(_dirname  , "../client/build");
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-app.use(express.static(buildPath))
-
-app.get("/*", function(req, res){
-    console.log(__dirname)
-    res.sendFile(
-        path.join(__dirname, "../client/build/index.html"),
-        function (err) {
-          if (err) {
-            res.status(500).send(err);
-          }
-        }
-      );
-
-})*/
-//////////////////////////////////////////////////////////////////////////
-
-const CONNECTION_URL = process.env.CONNECTION_URL   //setting connection url
-const PORT = process.env.PORT|| 5000; //setting port used to be process.env.PORT|| 5000
-
-io.on("connection", (socket) => {//socket.io chat capability
-    console.log(`User Connected: ${socket.id}`);
-  
-    socket.on("join_room", (data) => {
-      socket.join(data);
-      console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    });
-  
-    socket.on("send_message", (data) => {
-      socket.to(data.room).emit("receive_message", data);
-    });
-  
-    socket.on("disconnect", () => {
-      console.log("User Disconnected", socket.id);
-    });
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes2)
-app.use('/login', loginRoutes)
-app.use('/messages', messageRoutes)
-app.use('/pages', pageRoutes)
-app.use('/email', emailRoutes)
-app.use('/calendar', calendarRoutes)
-app.post('/webhook', webhookRoutes)
-app.use('/analytics', analyticsRoutes)
-app.use('/post', postRoutes)
-app.use('/api/user', userRoutes)
-app.use('/profiles', profileRoutes)
+app.use('/posts', postRoutes2);
+app.use('/login', loginRoutes);
+app.use('/messages', messageRoutes);
+app.use('/pages', pageRoutes);
+app.use('/email', emailRoutes);
+app.use('/calendar', calendarRoutes);
+app.post('/webhook', webhookRoutes);
+app.use('/analytics', analyticsRoutes);
+app.use('/post', postRoutes);
+app.use('/api/user', userRoutes);
+app.use('/profiles', profileRoutes);
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()  => server.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
     .catch((error) => console.log(error.message));
-    
